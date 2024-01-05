a2e373f9753eebb1d0adef71d587bbdb9bbac766
loki.odin <loki.odin@kakaostyle.com>
2024-01-05 12:15:21 +0900

    Initial commit

---
+++ src/main/kotlin/com/kakaostyle/inboundreviewsync/InboundReviewSyncApplication.kt
@@ -0,0 +1,11 @@
+package com.kakaostyle.inboundreviewsync
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InboundReviewSyncApplication
+
+fun main(args: Array<String>) {
+	runApplication<InboundReviewSyncApplication>(*args)
+}
---
+++ src/test/kotlin/com/kakaostyle/inboundreviewsync/InboundReviewSyncApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kakaostyle.inboundreviewsync
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InboundReviewSyncApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
